624c410cfda5c2d8c94e09938eed7c98d6dc8742
placid-tier-4550 <james.ardery@gmail.com>
2020-09-21 22:46:13 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,8 +2,8 @@ def find_max_value(array)
   counter = 0
   while counter < array.length do
     if array[counter] == array.max
-      return array[counter]
-    counter += 1
-  end
+      return array[counter]  
+   end
+  counter += 1
   end
 end
